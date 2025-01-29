4be360d9ea473a93d3fa67681346eff167a4b6d1
Harrycarter555 <top9pdf@gmail.com>
2025-01-29 21:09:12 +0530

    Update fsrcnn.py

--- fsrcnn.py
+++ fsrcnn.py
@@ -1,86 +1,52 @@
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-from __future__ import print_function
 import tensorflow as tf
-import numpy as np
-import cv2
-import os
 
-def model(x, y, lr_size, scale, batch, lr, dsm):
+def model(input_shape, scale, dsm):
     """
-    Implementation of FSRCNN: http://mmlab.ie.cuhk.edu.hk/projects/FSRCNN.html.
+    Implementation of FSRCNN using TensorFlow 2.x Keras API.
     """
-    # Unpack the tuple inside the function
-    d, s, m = dsm  
+    d, s, m = dsm  # Unpack the tuple
     channels = 1
     PS = channels * (scale * scale)  # for sub-pixel, PS = Phase Shift
-    bias_initializer = tf.constant_initializer(value=0.0)
 
-    # -- Filters and Biases
-    filters = [
-    tf.Variable(tf.random.normal([5, 5, 1, d], stddev=0.1), name="f1"),
-    tf.Variable(tf.random.normal([1, 1, d, s], stddev=0.1), name="f2"),
-    tf.Variable(tf.random.normal([1, 1, s, d], stddev=0.1), name="f%d" % (3 + m)),
-    tf.Variable(tf.random.normal([1, 1, d, PS], stddev=0.1), name="f%d" % (4 + m))
-]
+    # Input layer
+    x = tf.keras.Input(shape=input_shape, name="input_image")
 
-    bias = [
-        tf.get_variable(shape=[d], initializer=bias_initializer, name="b1"),
-        tf.get_variable(shape=[s], initializer=bias_initializer, name="b2"),
-        tf.get_variable(shape=[d], initializer=bias_initializer, name="b%d" % (3 + m)),
-        tf.get_variable(shape=[PS], initializer=bias_initializer, name="b%d" % (4 + m))
-    ]
-
-    # Add filters and biases for 'non-linear mapping' layers (depending on m)
-    for i in range(0, m):
-        filters.insert(i + 2, tf.Variable(tf.random_normal([3, 3, s, s], stddev=0.1), name="f%d" % (3 + i)))
-        bias.insert(i + 2, tf.get_variable(shape=[s], initializer=bias_initializer, name="b%d" % (3 + i)))
-
-    # -- Model architecture --
     # Feature extraction
-    x = tf.nn.conv2d(x, filters[0], [1, 1, 1, 1], padding='SAME', name="conv1")
-    x = x + bias[0]
-    x = prelu(x, "alpha1")
+    x = tf.keras.layers.Conv2D(d, kernel_size=5, strides=1, padding='same', activation=None, name="conv1")(x)
+    x = parametric_relu(x, "alpha1")
 
     # Shrinking
-    x = tf.nn.conv2d(x, filters[1], [1, 1, 1, 1], padding='SAME', name="conv2")
-    x = x + bias[1]
-    x = prelu(x, "alpha2")
+    x = tf.keras.layers.Conv2D(s, kernel_size=1, strides=1, padding='same', activation=None, name="conv2")(x)
+    x = parametric_relu(x, "alpha2")
 
-    # Non-linear mapping (amount of layers depends on m)
-    for i in range(0, m):
-        x = tf.nn.conv2d(x, filters[2 + i], [1, 1, 1, 1], padding='SAME', name="conv%d" % (3 + i))
-        x = x + bias[2 + i]
-        x = prelu(x, "alpha{}".format(3 + i))
+    # Non-linear mapping (m layers)
+    for i in range(m):
+        x = tf.keras.layers.Conv2D(s, kernel_size=3, strides=1, padding='same', activation=None, name=f"conv{i + 3}")(x)
+        x = parametric_relu(x, f"alpha{i + 3}")
 
     # Expanding
-    x = tf.nn.conv2d(x, filters[3 + (m - 1)], [1, 1, 1, 1], padding='SAME', name="conv%d" % (3 + m))
-    x = x + bias[3 + (m - 1)]
-    x = prelu(x, "alpha{}".format(3 + m))
+    x = tf.keras.layers.Conv2D(d, kernel_size=1, strides=1, padding='same', activation=None, name=f"conv{m + 3}")(x)
+    x = parametric_relu(x, f"alpha{m + 3}")
 
     # Final convolution (sub-pixel layer)
-    x = tf.nn.conv2d(x, filters[4 + (m - 1)], [1, 1, 1, 1], padding='SAME', name="conv%d" % (4 + m))
+    x = tf.keras.layers.Conv2D(PS, kernel_size=1, strides=1, padding='same', activation=None, name=f"conv{m + 4}")(x)
 
     # Sub-pixel (depth-to-space)
     x = tf.nn.depth_to_space(x, scale, data_format='NHWC')
-    out = tf.nn.bias_add(x, bias[4 + (m - 1)], name="NHWC_output")
 
-    # -- Outputs --
-    out_nchw = tf.transpose(out, [0, 3, 1, 2], name="NCHW_output")  # Transpose to NCHW format if needed
-    psnr = tf.image.psnr(out, y, max_val=1.0)  # Compute PSNR
-    loss = tf.losses.mean_squared_error(out, y)  # Compute MSE loss
-    train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)  # Optimizer
+    # Output layer
+    out = tf.keras.layers.Activation('linear', name="output")(x)
 
-    return out, loss, train_op, psnr
+    # Define the model
+    return tf.keras.Model(inputs=x, outputs=out)
 
-
-def prelu(_x, name):
+def parametric_relu(_x, name):
     """
     Parametric ReLU activation function.
     """
-    alphas = tf.get_variable(name, _x.get_shape()[-1], initializer=tf.constant_initializer(0.1), dtype=tf.float32, trainable=True)
-    pos = tf.nn.relu(_x)  # Positive part
-    neg = alphas * (_x - abs(_x)) * 0.5  # Negative part
+    alphas = tf.Variable(tf.constant(0.1, shape=[_x.shape[-1]]), name=name, trainable=True)
+    pos = tf.nn.relu(_x)
+    neg = alphas * (_x - tf.abs(_x)) * 0.5
     return pos + neg
 
 
